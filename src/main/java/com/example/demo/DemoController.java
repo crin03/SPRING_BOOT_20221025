d124706bc86d403e33c653d903ec838530e1426d
crin03 <jyt2097@gmail.com>
2024-10-02 12:25:35 +0900

    3주차 수업내용/연습문제 완료

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,14 +3,16 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller // 컨트롤러 어노테이션 명시
+
+
+@Controller
 public class DemoController {
-    @GetMapping("/hello") // 전송 방식 GET
-    
+    @GetMapping("/hello")
     public String hello(Model model) {
-        model.addAttribute("data", " 반갑습니다."); // model 설정
-        return "hello"; // hello.html 연결
+        model.addAttribute("data", "반갑습니다.");
+        return "hello";
     }
 
     // 2주차 연습문제
@@ -24,4 +26,12 @@ public class DemoController {
         model.addAttribute("data5", " 매우 좋습니다.");
         return "hello2"; 
     }
+
+    // 3주차 about_detailed
+    @GetMapping("/about_detailed")
+    public String about() {
+        return "about_detailed";
+    }
+
+    
 }
