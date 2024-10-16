1a954738357d4d2c46788cfe1a59945e7f762bcc
crin03 <jyt2097@gmail.com>
2024-10-16 12:50:41 +0900

    5주차 연습문제 추가

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -3,11 +3,16 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.model.domain.Article;
+import com.example.demo.model.service.AddArticleRequest;
 import com.example.demo.model.service.BlogService;
 
 @Controller
@@ -19,6 +24,13 @@ public class BlogController {
     public String article_list(Model model) {
     List<Article> list = blogService.findAll(); // 게시판 리스트
     model.addAttribute("articles", list); // 모델에 추가
-    return "article_list"; // .HTML 연결
+    return "article_list"; // HTML 연결
+    }
+
+    // 5주차 연습문제
+    @PostMapping("/api/articles")
+    public String addArticle(@ModelAttribute AddArticleRequest request) {
+        blogService.save(request);
+        return "redirect:/article_list";
     }
 }
--- src/main/java/com/example/demo/controller/BlogRestController.java
+++ src/main/java/com/example/demo/controller/BlogRestController.java
@@ -1,26 +1,26 @@
 package com.example.demo.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.model.domain.Article;
-import com.example.demo.model.service.AddArticleRequest;
-import com.example.demo.model.service.BlogService;
+// import com.example.demo.model.domain.Article;
+// import com.example.demo.model.service.AddArticleRequest;
+// import com.example.demo.model.service.BlogService;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController // @Controller + @ResponseBody
 public class BlogRestController {
-    private final BlogService blogService;
+    // private final BlogService blogService;
 
-    @PostMapping("/api/articles")
-    public ResponseEntity<Article> addArticle(@ModelAttribute AddArticleRequest request) {
-        Article saveArticle = blogService.save(request);
-        return ResponseEntity.status(HttpStatus.CREATED)
-        .body(saveArticle);
-    }
+    // @PostMapping("/api/articles")
+    // public ResponseEntity<Article> addArticle(@ModelAttribute AddArticleRequest request) {
+    //     Article saveArticle = blogService.save(request);
+    //     return ResponseEntity.status(HttpStatus.CREATED)
+    //     .body(saveArticle);
+    // }
 
     @GetMapping("/favicon.ico")
     public void favicon() {
