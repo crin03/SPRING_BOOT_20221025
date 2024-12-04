6b8a73de4ada72216ad3db544566895bc04afd17
crin03 <jyt2097@gmail.com>
2024-12-04 12:55:23 +0900

    9주차 입력값 필터링 추가

--- src/main/java/com/example/demo/model/service/AddMemberRequest.java
+++ src/main/java/com/example/demo/model/service/AddMemberRequest.java
@@ -1,17 +1,34 @@
 package com.example.demo.model.service;
 
 import com.example.demo.model.domain.Member;
+
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @NoArgsConstructor // 기본 생성자 추가
 @AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 추가
 @Data 
 public class AddMemberRequest {
+    @NotBlank // 공백x
+    @Pattern(regexp = "^[a-zA-Z가-힣]+$") // 특수문자x
     private String name;
+
+    @NotBlank // 공백x
+    @Email // 이메일 형식
     private String email;
+
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).{8,}$") // 패턴(8글자이상, 대소문자 포함)
     private String password;
+
+    @Pattern(regexp = "^\\d+$") // 패턴(숫자만 가능)
+    @Min(value = 19) // 19이상
+    @Max(value = 90) // 90이하
     private String age;
+
+    @NotEmpty // 공백은 허용, Null과 빈 문자열 허용x
     private String mobile;
+
+    @NotEmpty // 공백은 허용, Null과 빈 문자열 허용x
     private String address;
 
     public Member toEntity(){ // Member 생성자를 통해 객체 생성
